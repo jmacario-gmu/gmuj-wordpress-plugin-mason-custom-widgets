96516d50ae362fa7cbb808cac6dafe0fc20c4c3c
Jan Macario <jmacario@GMU.EDU>
2020-06-09 15:22:40 -0400

    Initial commit.

---
+++ gmuj-wordpress-plugin-mason-custom-widgets.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Summary: Main plugin file for the Mason WordPress: Custom Widgets plugin
+ */
+
+/**
+ * Plugin Name:       Mason WordPress: Custom Widgets
+ * Author:            Jan Macario
+ * Plugin URI:        https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-custom-widgets
+ * Description:       Mason WordPress plugin which implements Mason-specific custom widgets.
+ * Version:           0.0.1
+ */
+
+
+// Exit if this file is called directly.
+	if (!defined('WPINC')) {
+		die;
+	}
+
+
