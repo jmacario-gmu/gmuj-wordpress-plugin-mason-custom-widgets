31fa5130a0fc9c7bc714aebef11ad3ebacb1cd58
Jan Macario <jmacario@GMU.EDU>
2020-08-07 00:08:59 -0400

    Updates version number.

--- gmuj-wordpress-plugin-mason-custom-widgets.php
+++ gmuj-wordpress-plugin-mason-custom-widgets.php
@@ -9,7 +9,7 @@
  * Author:            Jan Macario
  * Plugin URI:        https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-custom-widgets
  * Description:       Mason WordPress plugin which implements Mason-specific custom widgets.
- * Version:           0.0.1
+ * Version:           0.0.9
  */
 
 
